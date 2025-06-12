4bbf632632a33712e548e9dd859d23bcc0ed4210
Dominik Hegedus <hege0123@gmail.com>
2025-06-12 22:32:28 +0200

    modify redis config

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -5,4 +5,4 @@ if (!process.env.REDIS_URL) {
   throw new Error("REDIS_URL environment variable is not set");
 }
 
-export const redis = new Redis(process.env.REDIS_URL!);
+export const redis = new Redis(process.env.REDIS_URL! + "?family=0");
