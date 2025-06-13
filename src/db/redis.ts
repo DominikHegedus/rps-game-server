a63740ea0bac419b0a6998645aadb770b0df6e41
Dominik Hegedus <hege0123@gmail.com>
2025-06-13 09:44:19 +0200

    family

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -5,4 +5,5 @@ if (!process.env.REDIS_URL) {
   throw new Error("REDIS_URL environment variable is not set");
 }
 
+// Family 0 is for IPv4
 export const redis = new Redis(process.env.REDIS_URL! + "?family=0");
