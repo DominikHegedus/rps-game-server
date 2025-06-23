28e63dae202fcd787dd0305100f37308b5d59d66
Dominik Hegedus <hege0123@gmail.com>
2025-06-23 23:16:14 +0200

    add jsdoc

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import { Redis } from "ioredis";
 
+/**
+ * MATCHMAKING
+ */
+
 if (!process.env.MATCHMAKING_REDIS_URL) {
   throw new Error("MATCHMAKING_REDIS_URL environment variable is not set");
 }
@@ -10,12 +14,20 @@ export const matchmakingRedis = new Redis(
   process.env.MATCHMAKING_REDIS_URL! + "?family=0"
 );
 
+/**
+ * ROOM
+ */
+
 if (!process.env.ROOM_REDIS_URL) {
   throw new Error("ROOM_REDIS_URL environment variable is not set");
 }
 
 // Family 0 is for IPv4
 export const roomRedis = new Redis(process.env.ROOM_REDIS_URL! + "?family=0");
+
+/**
+ * ROUND TIMER
+ */
 
 if (!process.env.ROUND_TIMERS_REDIS_URL) {
   throw new Error("ROUND_TIMERS_REDIS_URL environment variable is not set");
